df53edfe61bb1407a8522bdf8946a8854726805b
Midssouza <michelle.ubuntulh@gmail.com>
2023-05-10 16:18:50 -0300

    projeto ecommerce

---
+++ workspace-spring-tool-suite-4-4.18.1.RELEASE/lojasalva1real/src/main/java/com/lojasalva1real/Lojasalva1realApplication.java
@@ -0,0 +1,13 @@
+package com.lojasalva1real;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lojasalva1realApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lojasalva1realApplication.class, args);
+	}
+
+}
---
+++ workspace-spring-tool-suite-4-4.18.1.RELEASE/lojasalva1real/src/test/java/com/lojasalva1real/Lojasalva1realApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lojasalva1real;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lojasalva1realApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
